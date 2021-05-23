81c78a875410884a53c11a60f9e213b364322e43
ryotaro kurita <ryo.kuri2525@gmail.com>
2021-05-23 20:38:03 +0900

    php with mysql

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!-- saved from url=(0038)http://localhost:8888/php/sample21.php -->
+<html lang="ja"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!-- Required meta tags -->
+
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="./PHP_files/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>郵便番号987-6543</pre>
+</main>
+    
+</body></html>
