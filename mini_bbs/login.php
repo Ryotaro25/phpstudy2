970b394c8c676a8e6dfd543702c8a5c26af2d8cc
ryotaro kurita <ryo.kuri2525@gmail.com>
2021-05-26 07:05:34 +0900

    add colkie to login

--- mini_bbs/login.php
+++ mini_bbs/login.php
@@ -2,17 +2,29 @@
 session_start();
 require('dbconnect.php');
 
+if ($_COOKIE['email'] !== '') {
+  $email = $_COOKIE['email'];
+}
+
 if (!empty($_POST)) {
+  $email = $_POST['email'];
   if ($_POST['email'] !== '' && $_POST['password'] !== '') {
     $login = $db->prepare('SELECT * FROM members WHERE email=? AND password=?');
     $login->execute(array(
       $_POST['email'],
       sha1($_POST['password'])
     ));
+  
     $member = $login->fetch();
+
     if ($member) {
       $_SESSION['id'] = $member['id'];
       $_SESSION['time'] = time();
+
+      if ($_POST['save'] === 'on') {
+        setcookie('email', $_POST['email'], time()+60*60*24*14);
+      }
+
       header('Location: index.php');
       exit();
     } else {
@@ -48,7 +60,7 @@ if (!empty($_POST)) {
       <dl>
         <dt>メールアドレス</dt>
         <dd>
-          <input type="text" name="email" size="35" maxlength="255" value="<?php print (htmlspecialchars($_POST['email'], ENT_QUOTES)); ?>" />
+          <input type="text" name="email" size="35" maxlength="255" value="<?php print (htmlspecialchars($email, ENT_QUOTES)); ?>" />
           <?php if ($error['login'] === 'blank') : ?>
             <p class="error">メールアドレスとパスワードを記入してください</p>
           <?php endif; ?>
